2e552ff36e22f27decaca7e8d31156a1fbfdb2b3
TomJuddy <130992643+TomJuddy@users.noreply.github.com>
2023-04-17 15:56:55 +0100

    Update Navbar.jsx

--- my-site/components/Navbar.jsx
+++ my-site/components/Navbar.jsx
@@ -1,15 +1,29 @@
 import React from "react";
-import { Link } from "react-scroll";
+import Link from "next/link";
 import {ImHome} from "react-icons/im";
+import {BsCodeSlash,BsFillPersonLinesFill} from "react-icons/bs";
+import {HiOutlineMail} from "react-icons/hi";
+
 
 const Navbar = () => {
     return(
-    <nav className="fixed top-4 lg:bottom-8 w-full overflow-hidden">
+    <nav className="fixed top-10 lg:bottom-8 w-full overflow-hidden">
     <div className="container mx-auto">
-    <div className="w-full square border-2 border-light h-[60px] rounded-xl max-w-[550px] max-auto px-5 flex justify-between mr-auto ml-auto text-light/90">
+    <div className="w-full square border-2 border-dark h-[px] rounded-xl max-w-[570px] max-auto px-5 flex justify-between ml-auto  text-gray-300">
     <Link href="/" className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
-    <ImHome className="h-10 w-10"/>
+    <ImHome className="h-20 w-20"/>
     </Link>
+    <Link href="/Projects" className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+    <BsCodeSlash className="h-20 w-20"/>
+    </Link>
+    <Link href="/About" className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+    <BsFillPersonLinesFill className="h-20 w-20"/>
+    </Link>
+    <Link href="/Contact" className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+    <HiOutlineMail className="h-20 w-20"/>
+    </Link>
+   
+    
       </div>
     </div>
   </nav> 
